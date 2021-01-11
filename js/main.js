1f5baa9104b81433a44f6dfe01bf73306e06e3af
yukikohiga-sys <75060610+yukikohiga-sys@users.noreply.github.com>
2021-01-11 14:25:31 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+{
+    let t = 0;
+
+
+    function draw() {
+      const canvas = document.querySelector('canvas');
+      if (typeof canvas.getContext === 'undefined') {
+        return;
+      }
+      const ctx = canvas.getContext('2d');
+
+      ctx.clearRect(0,0,canvas.width,canvas.height);
+
+      ctx.beginPath();
+      ctx.ellipse(100,100,40,30,0,0,2*Math.PI);
+      ctx.fillStyle = 'black';
+      ctx.fill();
+    
+      ctx.beginPath();
+      ctx.ellipse(80 + Math.sin(t / 30),100,8,8,0,0,2*Math.PI);
+      ctx.ellipse(120 + Math.sin(t / 30),100,8,8,0,0,2*Math.PI);
+      ctx.fillStyle = 'skyblue';
+      ctx.fill();
+
+      t++;
+      setTimeout(draw,10);
+      
+      
+    }
+  
+    draw();
+  }
