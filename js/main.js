df52ae1d6e04b3a5ba4a705fa05d48309ead75d7
yukikohiga-sys <75060610+yukikohiga-sys@users.noreply.github.com>
2021-01-23 18:07:13 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,34 +1,124 @@
 'use strict';
 
-{
-    let t = 0;
+  (() => {
+     class Ball {
+      constructor(canvas) {
+         this.canvas = canvas;
+         this.ctx = this.canvas.getContext('2d');
+         this.x = 30;
+         this.y = 30;
+         this.r = 10;
+         this.x = this.pointA + 20;
+         this.addClick();
 
-
-    function draw() {
-      const canvas = document.querySelector('canvas');
-      if (typeof canvas.getContext === 'undefined') {
-        return;
       }
-      const ctx = canvas.getContext('2d');
 
-      ctx.clearRect(0,0,canvas.width,canvas.height);
+      addClick() {
+         document.addEventListener('click' , e => {
+           this.pointA = e.clientX;
 
-      ctx.beginPath();
-      ctx.ellipse(100,100,40,30,0,0,2*Math.PI);
-      ctx.fillStyle = 'black';
-      ctx.fill();
+         });
+
+      }
+      
+      update() {
+         this.x = this.pointA;
+      
+
+      }
+
+      draw() {
+        this.ctx.beginPath();
+        this.ctx.fillStyle = '#fdfdfd';
+        this.ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        this.ctx.fill();
+      }
+     }
+
+
+
+     class Game {
+       constructor(canvas) {
+         this.canvas = canvas;
+         this.ctx = this.canvas.getContext('2d');
+         this.ball = new Ball(this.canvas);
+         this.loop();
+       }    
+
+       loop() {
+         this.update();
+         this.draw();
+
+         requestAnimationFrame(() => {
+           this.loop();
+
+         });
+
+       }
+
+       update() {
+         this.ball.update();
+
+
+       }
+
+       draw() {
+        this.ball.draw();
+
+       }
+     }
+
+
+    const canvas = document.querySelector('canvas');
+    if (typeof canvas.getContext === 'undefined') {
+       return;
+        }  
+
+    new Game(canvas);
     
-      ctx.beginPath();
-      ctx.ellipse(80 + Math.sin(t / 30),100,8,8,0,0,2*Math.PI);
-      ctx.ellipse(120 + Math.sin(t / 30),100,8,8,0,0,2*Math.PI);
-      ctx.fillStyle = 'skyblue';
-      ctx.fill();
 
-      t++;
-      setTimeout(draw,10);
-      
-      
-    }
-  
-    draw();
-  }
+   })();
+    
+
+    // ctx.beginPath();
+    // //ctX.arc(x,y,r,start,end);
+    // ctx.arc(100,100,50,0,2 * Math.PI);
+    // ctx.stroke();
+
+
+
+
+
+    
+    
+    
+     // ctx.beginPath();
+    // ctx.moveTo(50,50);
+    // ctx.lineTo(100,50);
+    // ctx.lineTo(100,100);
+    // ctx.closePath();
+    // ctx.stroke();
+
+
+
+
+
+    //  ctx.shadowOffsetX = 4;
+    //  ctx.shadowOffsetY = 4;
+    //  ctx.shadowBlur =4;
+    //  ctx.shadowColor ='rgba(0,0,0,0.3)';
+     
+    //  ctx.fillRect(50,50,100,100);
+
+
+     //ctx.fillRect(x,y,width,height);
+//     ctx.fillStyle ='pink';
+//     ctx.strokeStyle = '#f00';
+   
+//     ctx.fillRect(50,50,50,50);
+//     ctx.strokeRect(50,50,50,50);
+
+//     ctx.fillStyle = 'skyblue';
+
+//     ctx.fillRect(70,70,50,50);
+//     ctx.strokeRect(70,70,50,50);
