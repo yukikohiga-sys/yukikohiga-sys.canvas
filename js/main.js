512d52964eebf9bc90ee6a9575278671d54ec60a
yukikohiga-sys <75060610+yukikohiga-sys@users.noreply.github.com>
2021-01-27 10:26:19 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,83 +1,158 @@
 'use strict';
 
-  (() => {
-     class Ball {
-      constructor(canvas) {
-         this.canvas = canvas;
-         this.ctx = this.canvas.getContext('2d');
-         this.x = 30;
-         this.y = 30;
-         this.r = 10;
-         this.x = this.pointA + 20;
-         this.addClick();
-
-      }
-
-      addClick() {
-         document.addEventListener('click' , e => {
-           this.pointA = e.clientX;
-
-         });
-
-      }
-      
-      update() {
-         this.x = this.pointA;
-      
-
-      }
-
-      draw() {
-        this.ctx.beginPath();
-        this.ctx.fillStyle = '#fdfdfd';
-        this.ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-        this.ctx.fill();
-      }
-     }
+  
 
 
 
-     class Game {
-       constructor(canvas) {
-         this.canvas = canvas;
-         this.ctx = this.canvas.getContext('2d');
-         this.ball = new Ball(this.canvas);
-         this.loop();
-       }    
-
-       loop() {
-         this.update();
-         this.draw();
-
-         requestAnimationFrame(() => {
-           this.loop();
-
-         });
-
-       }
-
-       update() {
-         this.ball.update();
 
 
-       }
-
-       draw() {
-        this.ball.draw();
-
-       }
-     }
 
 
-    const canvas = document.querySelector('canvas');
-    if (typeof canvas.getContext === 'undefined') {
-       return;
-        }  
 
-    new Game(canvas);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    const target1= document.getElementById('target1');
+    target1.classList.toggle('circle');
+    
+    target1.addEventListener('click', () => {
+     target1.classList.toggle('circle');
+
+
+    });
     
 
-   })();
+ 
+    const target2= document.getElementById('target2');
+    target2.classList.toggle('circle2');
+    
+    target2.addEventListener('click', () => {
+     target2.classList.toggle('circle2');
+
+
+    });
+
+    
+
+
+
+
+
+
+    
+    
+    
+    // for (let i = 0; i < 12; i++) {
+   
+    // });
+    
+    // const target1= document.getElementById('target1');
+    // const target2= document.getElementById('target2');
+    
+    
+    // target1.addEventListener('click', () => {
+    // target1.classList.toggle('circle');
+    // });
+    // target2.addEventListener('click', () => {
+    // target2.classList.toggle('circle2');
+    // });
+    
+
+
+
+  // (() => {
+  //    class Ball {
+  //     constructor(canvas) {
+  //        this.canvas = canvas;
+  //        this.ctx = this.canvas.getContext('2d');
+  //        this.x = 30;
+  //        this.y = 30;
+  //        this.r = 10;
+  //        this.x = this.pointA + 20;
+  //        this.addClick();
+
+  //     }
+
+  //     addClick() {
+  //        document.addEventListener('click' , e => {
+  //          this.pointA = e.clientX;
+
+  //        });
+
+  //     }
+      
+  //     update() {
+  //        this.x = this.pointA;
+      
+
+  //     }
+
+  //     draw() {
+  //       this.ctx.beginPath();
+  //       this.ctx.fillStyle = '#fdfdfd';
+  //       this.ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+  //       this.ctx.fill();
+  //     }
+  //    }
+
+
+
+  //    class Game {
+  //      constructor(canvas) {
+  //        this.canvas = canvas;
+  //        this.ctx = this.canvas.getContext('2d');
+  //        this.ball = new Ball(this.canvas);
+  //        this.loop();
+  //      }    
+
+  //      loop() {
+  //        this.update();
+  //        this.draw();
+
+  //        requestAnimationFrame(() => {
+  //          this.loop();
+
+  //        });
+
+  //      }
+
+  //      update() {
+  //        this.ball.update();
+
+
+  //      }
+
+  //      draw() {
+  //       this.ball.draw();
+
+  //      }
+  //    }
+
+
+  //   const canvas = document.querySelector('canvas');
+  //   if (typeof canvas.getContext === 'undefined') {
+  //      return;
+  //       }  
+
+  //   new Game(canvas);
+    
+
+  //  })();
     
 
     // ctx.beginPath();
